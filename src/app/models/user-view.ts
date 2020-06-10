0447711bee3b1c1ae284bfe80c4863fd2886cf68
Thomas Hansen <thomas@gaiasoul.com>
2020-06-10 09:42:15 +0300

    Bettering

--- src/app/models/user-view.ts
+++ src/app/models/user-view.ts
@@ -3,10 +3,11 @@
  * Model for accepting a case and persisting to backend.
  */
 export class UserView {
-    usernameL: string;
+    username: string;
     status: string;
     name: string;
     cases: number;
     positive: number;
     negative: number;
+    regions: string[];
 }
