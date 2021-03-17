c5479bf6485e1acfe4c4baa906a520184a7d9985
Thomas Hansen <thomas@gaiasoul.com>
2021-03-17 09:42:58 +0200

    Update anarq.service.ts

--- src/app/services/anarq.service.ts
+++ src/app/services/anarq.service.ts
@@ -30,7 +30,9 @@ export class AnarqService {
        * @returns A single page object
        */
       get: (url: string) => {
-        return this.httpClient.get(environment.apiUrl + 'magic/modules/anarq/site/page?url=' + encodeURIComponent(url));
+        return this.httpClient.get(
+          environment.apiUrl + 'magic/modules/anarq/site/page?url=' +
+          encodeURIComponent(url));
       },
 
       /**
@@ -39,7 +41,8 @@ export class AnarqService {
        * @returns All pages in your database
        */
       list: () => {
-        return this.httpClient.get(environment.apiUrl + 'magic/modules/anarq/site/pages');
+        return this.httpClient.get(
+          environment.apiUrl + 'magic/modules/anarq/site/pages');
       },
 
       /**
@@ -51,10 +54,11 @@ export class AnarqService {
        * @returns Whether or not creation was successful
        */
       create: (url: string, name: string, content: string) => {
-        return this.httpClient.post(environment.apiUrl + 'magic/modules/anarq/site/page', {
-          url,
-          name,
-          content,
+        return this.httpClient.post(
+          environment.apiUrl + 'magic/modules/anarq/site/page', {
+            url,
+            name,
+            content,
         });
       },
 
@@ -67,10 +71,11 @@ export class AnarqService {
        * @returns Whether or not creation was successful
        */
        update: (url: string, name: string, content: string) => {
-        return this.httpClient.put(environment.apiUrl + 'magic/modules/anarq/site/page', {
-          url,
-          name,
-          content,
+        return this.httpClient.put(
+          environment.apiUrl + 'magic/modules/anarq/site/page', {
+            url,
+            name,
+            content,
         });
       },
 
@@ -81,8 +86,88 @@ export class AnarqService {
        * @returns Whether or not invocation was successful or not
        */
       delete: (url: string) => {
-        return this.httpClient.delete(environment.apiUrl + 'magic/modules/anarq/site/page?url=' + encodeURIComponent(url));
+        return this.httpClient.delete(
+          environment.apiUrl + 'magic/modules/anarq/site/page?url=' +
+          encodeURIComponent(url));
       }
     };
+  }
+
+  /**
+   * Gives you access to profiles, authenticating users, registering new users, etc.
+   */
+  get profile() {
+    return {
+
+      /**
+       * Authenticates a user returning an authorisation token back to caller.
+       * 
+       * @param username Username
+       * @param password Password
+       * @returns An authorisation token needed to authorise user in consecutive requests
+       */
+      authenticate: (username: string, password: string) => {
+        return this.httpClient.get(
+          environment.apiUrl + 'magic/modules/anarq/profile/authenticate?username=' +
+          encodeURIComponent(username) + '&password=' + encodeURIComponent(password));
+      },
+
+      /**
+       * Allows a user to confirms his or her email address.
+       * 
+       * @param email Email address of user
+       * @param secret Secret sent to user's email address
+       * @returns Whether or not confirmation was successful or not
+       */
+      confirmEmail: (email: string, secret: string) => {
+        return this.httpClient.post(
+          environment.apiUrl + 'magic/modules/anarq/profile/confirm-email', {
+            email,
+            secret,
+          }
+        );
+      },
+
+      /**
+       * Checks if an email address is available or not.
+       * 
+       * @param email Email address to check
+       * @returns Success if email address is available
+       */
+      emailAvailable: (email: string) => {
+        return this.httpClient.get(
+          environment.apiUrl + 'magic/modules/anarq/profile/email-available' +
+          encodeURIComponent(email));
+      },
+
+      /**
+       * Returns profile information for the currently authenticated user.
+       * 
+       * @returns Information for the currently authenticated user
+       */
+      me: () => {
+        return this.httpClient.get(
+          environment.apiUrl + 'magic/modules/anarq/profile/me');
+      },
+
+      /**
+       * Registers a new user on site.
+       * 
+       * @param username Chosen username
+       * @param password Chosen password
+       * @param email Email of user
+       * @param fullName Full name of user
+       * @returns Whether or not operation was successful or not
+       */
+      register: (username: string, password: string, email: string, fullName: string) => {
+        return this.httpClient.post(environment.apiUrl + 'magic/modules/anarq/profile/register', {
+          username,
+          password,
+          email,
+          full_name: fullName,
+          email_type: 'web'
+        });
+      },
+    }
   }
 }
